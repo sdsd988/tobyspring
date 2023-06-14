01020fc490ce2df11e4fddd5820f7e3a77ceee8f
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-14 14:18:36 +0900

    FrontController 전환

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,26 +1,14 @@
 package tobyspring.helloboot;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    private final HelloService helloService;
-
-
-    public HelloController(HelloService helloService) {
-        this.helloService = helloService;
-    }
 
     @GetMapping("/hello")
-    public String hello(String name) {
-        //컨트롤러의 역할 중 하나는 클라이언트의 요청 사항을 검증하는 것이다.
-        if (name == null || name.trim().length() == 0) throw new IllegalArgumentException();
-        return helloService.sayHello(name);
-    }
-
-    @GetMapping("/count")
-    public String count(String name) {
-        return name + ":" + helloService.countOf(name);
+    public String hello(String name){
+        return "Hello " + name;
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,29 +1,50 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@SpringBootApplication
-public class HellobootApplication  {
-
-	private final JdbcTemplate jdbcTemplate;
-
-	public HellobootApplication(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-
-	@PostConstruct
-	void init(){
-		jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-	}
+public class HellobootApplication {
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    //인증, 보안, 다국어, 공통 기능
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+                        String name = req.getParameter("name");
 
-		SpringApplication.run(HellobootApplication.class, args);
-	}
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello " + name);
+                    } else if(req.getRequestURI().equals("/user")){
+
+                    }
+                    else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+                }
+            }).addMapping("/*");
+        });
+        webServer.start();
+    }
 
 }
