ad7a30fcf5805932e52968073fe43576789399f1
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-14 23:40:15 +0900

    Bean의 생명주기 메소드, Api 테스트 코드 작성

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,25 +1,32 @@
 package tobyspring.helloboot;
 
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
-@RequestMapping("/hello")
-public class HelloController {
+@RestController
+public class HelloController  {
 
     //컨트롤러의 역할 : 유저의 요청사항을 검증하는 것
 
     private final HelloService helloService;
+    private  ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
 
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,52 +1,33 @@
 package tobyspring.helloboot;
 
-import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+@Configuration
+@ComponentScan
 public class HellobootApplication {
 
+    @Bean
+    public ServletWebServerFactory serverFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet(){
+        return new DispatcherServlet();
+    }
 
     public static void main(String[] args) {
-        //스프링 컨테이너 생성
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
 
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-                    servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(this)
-                    ).addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
-
-
+        SpringApplication.run(HellobootApplication.class, args);
     }
+
 
 }
