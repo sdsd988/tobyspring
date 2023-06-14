5fd61ea929f1a32050ed218d043302d28b94573e
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-14 14:31:06 +0900

    컨트롤러 매핑과 바인딩

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -25,16 +25,20 @@ public class HellobootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     //인증, 보안, 다국어, 공통 기능
                     if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-                        String name = req.getParameter("name");
+                        String name = req.getParameter("name"); // 바인딩
+
+                        String ret = helloController.hello(name);
 
                         resp.setStatus(HttpStatus.OK.value());
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello " + name);
+                        resp.getWriter().println(ret);
                     } else if(req.getRequestURI().equals("/user")){
 
                     }
