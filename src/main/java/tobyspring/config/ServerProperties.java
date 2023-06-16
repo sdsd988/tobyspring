d4006d1d96dcab81e93a03df4601f1d4c73b0d1b
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-16 11:03:18 +0900

    환경변수 지정 및 프로퍼티 클래스 분리

---
+++ src/main/java/tobyspring/config/ServerProperties.java
@@ -0,0 +1,26 @@
+package tobyspring.config;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class ServerProperties {
+
+    String contextPath;
+
+    int port;
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,25 +1,35 @@
 package tobyspring.config.autoconfig;
 
 import com.fasterxml.jackson.databind.util.ClassUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
+import tobyspring.config.ServerProperties;
 
 @MyAutoConfiguration
 //@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory serverFactory(){
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory serverFactory(ServerProperties properties){
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+        return factory;
     }
+
+
 
 //    static class TomcatCondition implements Condition {
 //
